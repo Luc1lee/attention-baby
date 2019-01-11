4509e27f9998953b2880077a4a78286280e90738
ink-feather <723220530@qq.com>
2019-01-11 16:10:23 +0800

    token的完善

--- src/main/java/com/dlx/ababy/tokenVerify/TokenInterceptor.java
+++ src/main/java/com/dlx/ababy/tokenVerify/TokenInterceptor.java
@@ -1,5 +1,9 @@
 package com.dlx.ababy.tokenVerify;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -7,12 +11,21 @@ import javax.servlet.http.HttpServletResponse;
 
 public class TokenInterceptor implements HandlerInterceptor {
 
+
     @Override
     public  boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         response.setCharacterEncoding("utf-8");
-        String token = request.getHeader("accessToken");
+        String token = request.getHeader("token");
         if (token != null && !token.equals("")) {
-            return true;
+
+            boolean result = JwtVerify.verify(token);
+
+            if (result) {
+                return true;
+            } else {
+                return false;
+            }
+
         } else {
             return false;
         }
